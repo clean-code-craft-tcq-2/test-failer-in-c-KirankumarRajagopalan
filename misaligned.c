793f59b11c378be69c25650832daa1d50a7219bc
KirankumarRajagopalan <91051522+KirankumarRajagopalan@users.noreply.github.com>
2022-01-26 11:26:57 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -4,27 +4,34 @@
 const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
+//Finding the number of minor and major colors
 int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
+//Define Color Pair structure
 typedef struct {
+    int pairNumber;
     const char* majorColor;
     const char* minorColor;
 } ColorPair;
 
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
-    colorPair.majorColor = MajorColorNames[(pairNumber / numberOfMinorColors)];
+    colorPair.pairNumber = pairNumber;
+    colorPair.majorColor = MajorColorNames[(pairNumber / numberOfMajorColors)];
     colorPair.minorColor = MinorColorNames[(pairNumber % numberOfMinorColors)];
     return colorPair;
 }
 
+//Test function to assert the major and minor color obtained from pair number.
 void testNumberToPair(int pairNumber, const char* expectedMajor, const char* expectedMinor){
     ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    assert(colorPair.pairNumber == pairNumber);
     assert(colorPair.majorColor == expectedMajor);
     assert(colorPair.minorColor == expectedMinor);
 }
 
+//Print and test the color map information
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
