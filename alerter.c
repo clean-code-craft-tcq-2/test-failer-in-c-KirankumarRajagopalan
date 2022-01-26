1e25ce97095e93459fc3c6257d903f8504ea150f
KirankumarRajagopalan <91051522+KirankumarRajagopalan@users.noreply.github.com>
2022-01-26 10:34:48 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -22,6 +22,7 @@ int networkAlertStub(float celcius) {
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
+    #if(SW == SW_STUBBED_FOR_TEST)
     int returnCode = networkAlertStub(celcius);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
